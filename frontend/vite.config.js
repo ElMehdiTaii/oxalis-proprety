2963b475b578bd0d6e17a8fd468db6440cc4b67a
mehdi_taii <taiielmehdi21@gmail.com>
2025-12-16 16:15:33 +0100

    Fix Netlify configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/oxalis-proprety/',
+  base: process.env.NETLIFY ? '/' : '/oxalis-proprety/',
   server: {
     port: 3000,
     open: true
